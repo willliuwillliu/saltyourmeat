015adb899a429c1299d3892d638e6ed5b6269652
Will <willliuwillliu@gmail.com>
2020-06-30 16:49:16 -0700

    testing passing values into form through hidden

--- index.js
+++ index.js
@@ -50,7 +50,9 @@ document.addEventListener("DOMContentLoaded", () => {
       lastResult["meatWeight"]
     } ${lastResult["meatWeightSelection"]} of ${meatSelectionText}. This was:`;
   }
-  document.getElementById("alltext").value = lastResult;
+  document.getElementById("hiddenMetaData").value = lastResult;
+  document.getElementById("hiddenSaltSelection").value =
+    lastResult["saltSelection"];
   // Process feedback form
   const processForm = (form) => {
     const data = new FormData(form);
